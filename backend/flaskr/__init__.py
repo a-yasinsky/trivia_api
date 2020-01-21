3c996b0bf958148a7073957751d67d744d4dbe51
a-yasinsky <anatoly@yasinsky.pro>
2020-01-21 14:10:14 +0600

    added errorhandlers

--- backend/flaskr/__init__.py
+++ backend/flaskr/__init__.py
@@ -200,5 +200,20 @@ def create_app(test_config=None):
   Create error handlers for all expected errors
   including 404 and 422.
   '''
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+      "success": False,
+      "erorr": 404,
+      "message": "resource not found"
+    }), 404
+
+  @app.errorhandler(422)
+  def not_found(error):
+    return jsonify({
+      "success": False,
+      "erorr": 422,
+      "message": "unprocessable entity"
+    }), 422
 
   return app
